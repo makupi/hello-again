396f4f8adda54b49a9e41cba4e3275c5b60d76de
Markus Pischinger <makupi@icloud.com>
2024-06-02 15:00:10 +0200

    Add django-ext and faker, create populate script

---
+++ crm/scripts/populate_db.py
@@ -0,0 +1,35 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+
+from datetime import timezone
+from faker import Faker
+
+from crm.models import AppUser, Address, CustomerRelationship
+
+
+def run():
+    fake = Faker()
+    for _ in range(3_000_000):
+        address = Address.objects.create(
+            street=fake.street_name(),
+            street_number=fake.random_number(digits=5),
+            city_code=fake.postalcode(),
+            city=fake.city(),
+            country=fake.country(),
+        )
+
+        appuser = AppUser.objects.create(
+            address=address,
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            customer_id=fake.random_number(digits=10),
+            phone_number=fake.phone_number(),
+            birthday=fake.date_of_birth(),
+        )
+
+        _ = CustomerRelationship.objects.create(
+            appuser=appuser,
+            points=fake.random_number(digits=4),
+            last_activity=fake.date_time_this_year(before_now=True, tzinfo=timezone.utc),
+        )
