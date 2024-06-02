93e6ca66707c603ea69315e975678199842efb65
Markus Pischinger <makupi@icloud.com>
2024-06-02 18:06:30 +0200

    Update populate to use bulk_create

--- crm/scripts/populate_db.py
+++ crm/scripts/populate_db.py
@@ -6,8 +6,12 @@ from crm.models import AppUser, Address, CustomerRelationship
 
 def run():
     fake = Faker()
+    
+    addresses = []
+    appusers = []
+    crs = []
     for _ in range(3_000_000):
-        address = Address.objects.create(
+        address = Address(
             street=fake.street_name(),
             street_number=fake.random_number(digits=5),
             city_code=fake.postalcode(),
@@ -15,8 +19,7 @@ def run():
             country=fake.country(),
         )
 
-        appuser = AppUser.objects.create(
-            address=address,
+        appuser = AppUser(
             first_name=fake.first_name(),
             last_name=fake.last_name(),
             gender=fake.passport_gender(),
@@ -25,8 +28,18 @@ def run():
             birthday=fake.date_of_birth(),
         )
 
-        _ = CustomerRelationship.objects.create(
-            appuser=appuser,
+        cr = CustomerRelationship(
             points=fake.random_number(digits=4),
             last_activity=fake.date_time_this_year(before_now=True, tzinfo=timezone.utc),
         )
+        addresses.append(address)
+        appusers.append(appuser)
+        crs.append(cr)
+    
+    addresses = Address.objects.bulk_create(addresses)
+    for c, appuser in enumerate(appusers):
+        appuser.address = addresses[c]
+    appusers = AppUser.objects.bulk_create(appusers)
+    for c, cr in enumerate(crs):
+        cr.appuser = appusers[c]
+    CustomerRelationship.objects.bulk_create(crs)
